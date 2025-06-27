550d8367cf1d35a12f4259eb3f08272f504624cf
ganskem <ganskem@uni-hildesheim.de>
2025-06-27 23:12:20 +0200

    Remove Top ActionBar, Implement Dummy Leaderboard with styling.

--- app/src/main/java/de/uni_hildesheim/mump/ui/leaderBoard/LeaderBoardFragment.java
+++ app/src/main/java/de/uni_hildesheim/mump/ui/leaderBoard/LeaderBoardFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,8 +23,10 @@ public class LeaderBoardFragment extends Fragment {
         binding = FragmentLeaderboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textLeaderBoard;
-        leaderBoardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Initialize your leaderboard data here
+        // You can access views from your layout like:
+        // binding.btnFilter.setOnClickListener(v -> { /* filter logic */ });
+
         return root;
     }
 
