54691083090b9d1b2e742559d7f3da7c00d6bd1b
Adam Krafczyk <krafczyk@uni-hildesheim.de>
2025-06-27 23:37:07 +0200

    Dummy data

--- app/src/main/java/de/uni_hildesheim/mump/api/Api.java
+++ app/src/main/java/de/uni_hildesheim/mump/api/Api.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
@@ -16,8 +17,11 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import de.uni_hildesheim.mump.dto.CourseDto;
+import de.uni_hildesheim.mump.dto.UserDto;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class Api {
@@ -44,11 +48,24 @@ public class Api {
         return get("/course/" + id, CourseDto.class);
     }
 
+    public List<UserDto> getAllUsers() throws IOException {
+        return List.of(
+                new UserDto("adam", "a@dam", false, 1252, Set.of()),
+                new UserDto("marcel", "a@dam", false, 235, Set.of()),
+                new UserDto("michael", "a@dam", false, 2342, Set.of()),
+                new UserDto("ani", "a@dam", false, 3534, Set.of())
+        );
+    }
+
     private <T> T get(String endpoint, Type resultType) throws IOException {
         URL url = new URL(baseUrl + endpoint);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("accept", "application/json");
-        return GSON.fromJson(new InputStreamReader(con.getInputStream()), resultType);
+
+        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
+                .collect(Collectors.joining("\n"));
+
+        return GSON.fromJson(output, resultType);
     }
 
     private <T> T post(String endpoint, Object data, Type resultType) throws IOException {
